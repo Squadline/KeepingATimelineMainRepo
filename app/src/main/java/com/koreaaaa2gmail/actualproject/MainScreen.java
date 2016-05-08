d49c8a7d8307f60b007a9f76332c86261e4f5f51
Jimmy Quach <jgquach@ucsd.edu>
2016-05-07 23:48:12 -0700

    basic timeline stuff, looks ugly

--- app/src/main/java/com/koreaaaa2gmail/actualproject/MainScreen.java
+++ app/src/main/java/com/koreaaaa2gmail/actualproject/MainScreen.java
@@ -7,16 +7,35 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainScreen extends AppCompatActivity {
 
     private static Button accSettings;
+    private Firebase db;
+    private TextView t;
+    private Button b;
+    private EditText inp;
+    private Button adText;
+    private Button tl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        Firebase.setAndroidContext(this);
+        db = new Firebase("https://fiery-fire-8218.firebaseio.com/");
+
 
         String[] settings = {"Settings", "Log Out"};
         ListView myList = (ListView) findViewById(R.id.left_drawer);
@@ -35,6 +54,57 @@ public class MainScreen extends AppCompatActivity {
 
         //call the method here.
         OnClickButtonListener();
+        t = (TextView)findViewById(R.id.textUser);
+        b = (Button)findViewById(R.id.button2);
+        inp = (EditText)findViewById(R.id.editText);
+        adText = (Button)findViewById(R.id.button3);
+        tl = (Button)findViewById(R.id.button5);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db = new Firebase("https://fiery-fire-8218.firebaseio.com/Users");
+                Firebase swagref =  db.child("swag/FirstName");
+                swagref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String stuff = dataSnapshot.getValue(String.class);
+                        t.setText(stuff);
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+
+                    }
+                });
+            }
+        });
+
+        tl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newActivity = new Intent("com.koreaaaa2gmail.actualproject.Timelineshower");
+                startActivity(newActivity);
+            }
+        });
+
+        adText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Firebase r = new Firebase("https://fiery-fire-8218.firebaseio.com/Users/swag");
+                Firebase d = r.push();
+                Map<String, String> post = new HashMap<String, String>();
+                post.put("input", inp.getText().toString());
+                d.setValue(post);
+                r.child("FirstName").setValue(inp.getText().toString());
+            }
+        });
+
+
+
+
+
+
     }
 
     /**
---
+++ app/src/main/java/com/koreaaaa2gmail/actualproject/Timelineshower.java
@@ -0,0 +1,51 @@
+package com.koreaaaa2gmail.actualproject;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by boredguy88 on 5/5/2016.
+ */
+public class Timelineshower extends AppCompatActivity {
+
+    private Firebase db;
+    private ArrayList<String> holder = new ArrayList<String>();
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timelineshower);
+        Firebase.setAndroidContext(this);
+        db = new Firebase("https://fiery-fire-8218.firebaseio.com/Users/swag/Timelines");
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot time: dataSnapshot.getChildren()){
+                    holder.add((String) time.getKey());
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        ArrayAdapter<String> timelineAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, holder);
+        ListView list = (ListView) findViewById(R.id.listView);
+        list.setAdapter(timelineAdapter);
+
+    }
+
+}
