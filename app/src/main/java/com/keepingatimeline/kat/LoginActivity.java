369e25c9a8ce60ea8c5f08815483c2db25cb5e64
Trevor <tjuanita@ucsd.edu>
2016-05-12 02:23:38 -0700

    Changed Login Page Layout

--- app/src/main/java/com/keepingatimeline/kat/LoginActivity.java
+++ app/src/main/java/com/keepingatimeline/kat/LoginActivity.java
@@ -4,16 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 /* Written by: Louis Leung
    Main login screen, this is what launches on start
  */
 public class LoginActivity extends AppCompatActivity {
 
+    private Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = (Button)findViewById(R.id.loginButton);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newActivity = new Intent("com.keepingatimeline.kat.MainScreen");
+                startActivity(newActivity);
+            }
+        });
 
         /* for forgot password button, if they click it, goes to Change Password
         Activity
@@ -36,11 +49,7 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(newActivity);
             }
         });
-    }
-    /* on clicking log in button, direct to Main screen activity */
-    public void LoginSuccess(View view) {
-        Intent newActivity = new Intent("com.keepingatimeline.kat.MainScreen");
-        startActivity(newActivity);
-    }
 
+
+    }
 }
