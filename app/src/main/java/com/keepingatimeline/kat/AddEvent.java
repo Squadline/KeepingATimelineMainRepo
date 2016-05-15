85b4b31b98b20ca87b15399603233b5e76990ec7
Joshua Cheung <jlcheung@ucsd.edu>
2016-05-15 15:26:49 -0700

    AddPhotoEvent allows picture picking

--- app/src/main/java/com/keepingatimeline/kat/AddEvent.java
+++ app/src/main/java/com/keepingatimeline/kat/AddEvent.java
@@ -1,15 +1,22 @@
 package com.keepingatimeline.kat;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 
-public class AddEvent extends AppCompatActivity {
-    private View quoteView;
-    private View photoView;
-    private View textView;
-    private int shortAnimationDuration;
+public class AddEvent extends AppCompatActivity implements View.OnClickListener{
+    private static final int RESULT_LOAD_IMAGE = 1;
+    View quoteView;
+    View photoView;
+    View textView;
+    ImageView uploadedPhoto;
+    Button bImportPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +26,15 @@ public class AddEvent extends AppCompatActivity {
         quoteView = findViewById(com.keepingatimeline.kat.R.id.AddQuoteEvent);
         photoView = findViewById(com.keepingatimeline.kat.R.id.AddPhotoEvent);
         textView = findViewById(com.keepingatimeline.kat.R.id.AddTextEvent);
+        bImportPhoto = (Button)findViewById(R.id.importPhoto);
+        uploadedPhoto = (ImageView)findViewById(R.id.uploadedImage);
 
         //Hide the Photo and Text Views
         photoView.setVisibility(View.GONE);
         textView.setVisibility(View.GONE);
+
+        //Create onClickListeners for Buttons
+        bImportPhoto.setOnClickListener(this);
     }
 
     public void onRadioButtonClicked(View view) {
@@ -50,6 +62,26 @@ public class AddEvent extends AppCompatActivity {
                     quoteView.setVisibility(View.GONE);
                 }
                 break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.importPhoto:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            uploadedPhoto.setImageURI(selectedImage);
         }
     }
 }
