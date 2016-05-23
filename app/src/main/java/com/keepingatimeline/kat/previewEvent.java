4f6da3c0f44c609cf698d1d63e683e301b5fcea5
michaelshum123 <m1shum@ucsd.edu>
2016-05-22 18:30:16 -0700

    goes to main if logged in

--- app/src/main/java/com/keepingatimeline/kat/previewEvent.java
+++ app/src/main/java/com/keepingatimeline/kat/previewEvent.java
@@ -3,7 +3,7 @@ package com.keepingatimeline.kat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class previewEvent extends AppCompatActivity {
+public class PreviewEvent extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
